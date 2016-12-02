e4960787960dc5dbc3b60fed20a8ba63f148f4d0
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-02 20:27:56 +0200

    add news.php

--- www/NewsDB.class.php
+++ www/NewsDB.class.php
@@ -9,7 +9,7 @@ require_once "INewsDB.class.php";
 
 class NewsDB implements INewsDB
 {
-    const DB_NAME = "news.db";
+    const DB_NAME = "../news.db";
     private $_db = null;
 
     function __get($name)
@@ -22,6 +22,31 @@ class NewsDB implements INewsDB
     function __construct()
     {
         $this->_db = new SQLite3(self::DB_NAME);
+	    if(filesize(self::DB_NAME)==0){
+		    $sql = "CREATE TABLE msgs(
+			    id INTEGER PRIMARY KEY AUTOINCREMENT,
+				title TEXT,
+				category INTEGER,
+				description TEXT,
+				source TEXT,
+				datetime INTEGER
+				)
+				";
+		    $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+		    $sql ="CREATE TABLE category(
+				id INTEGER,
+				name TEXT
+				)
+		        ";
+		    $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+		    $sql ="INSERT INTO category(id, name)
+				SELECT 1 as id, 'Политика' as name
+				UNION SELECT 2 as id, 'Культура' as name
+				UNION SELECT 3 as id, 'Спорт' as name 
+		        ";
+		    $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+	    }
+
     }
     function __destruct()
     {
@@ -37,4 +62,3 @@ class NewsDB implements INewsDB
 
 
 }
-$news = new NewsDB();
