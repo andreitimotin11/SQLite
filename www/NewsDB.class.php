e3ce0e8fa1ce08a81b3b6fa296908e2ccbd84ee2
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-02 23:37:26 +0200

    formated code

--- www/NewsDB.class.php
+++ www/NewsDB.class.php
@@ -9,21 +9,22 @@ require_once "INewsDB.class.php";
 
 class NewsDB implements INewsDB
 {
-    const DB_NAME = "../news.db";
-    private $_db = null;
-
-    function __get($name)
-    {
-        if($name == "db"){
-            return $this->_db;
-        }
-        throw new Exception("Unknown property!");
-    }
-    function __construct()
-    {
-        $this->_db = new SQLite3(self::DB_NAME);
-	    if(filesize(self::DB_NAME)==0){
-		    $sql = "CREATE TABLE msgs(
+	const DB_NAME = "../news.db";
+	private $_db = null;
+	
+	function __get($name)
+	{
+		if ($name == "db") {
+			return $this->_db;
+		}
+		throw new Exception("Unknown property!");
+	}
+	
+	function __construct()
+	{
+		$this->_db = new SQLite3(self::DB_NAME);
+		if (filesize(self::DB_NAME) == 0) {
+			$sql = "CREATE TABLE msgs(
 			    id INTEGER PRIMARY KEY AUTOINCREMENT,
 				title TEXT,
 				category INTEGER,
@@ -32,31 +33,32 @@ class NewsDB implements INewsDB
 				datetime INTEGER
 				)
 				";
-		    $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
-		    $sql ="CREATE TABLE category(
+			$this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+			$sql = "CREATE TABLE category(
 				id INTEGER,
 				name TEXT
 				)
 		        ";
-		    $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
-		    $sql ="INSERT INTO category(id, name)
+			$this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+			$sql = "INSERT INTO category(id, name)
 				SELECT 1 as id, 'Политика' as name
 				UNION SELECT 2 as id, 'Культура' as name
 				UNION SELECT 3 as id, 'Спорт' as name 
 		        ";
-		    $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
-	    }
-
-    }
-    function __destruct()
-    {
-        unset($this->_db);
-    }
-    
-    function saveNews($title, $category, $description, $source)
-    {
-        $dt = time();
-	    $sql = "INSERT INTO msgs(
+			$this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+		}
+		
+	}
+	
+	function __destruct()
+	{
+		unset($this->_db);
+	}
+	
+	function saveNews($title, $category, $description, $source)
+	{
+		$dt = time();
+		$sql = "INSERT INTO msgs(
 							title, 
 							category, 
 							description, 
@@ -67,33 +69,45 @@ class NewsDB implements INewsDB
 					'$description', 
 					'$source', 
 					$dt)";
-	   return $this->_db->exec($sql);
-    }
-	private function db2Arr($data){
+		return $this->_db->exec($sql);
+	}
+	
+	private function db2Arr($data)
+	{
 		$arr = [];
 		while ($row = $data->fetchArray(SQLITE3_ASSOC))
 			$arr[] = $row;
 		return $arr;
 	}
-    function getNews(){
-		$sql  = "
+	
+	function getNews()
+	{
+		$sql = "
 				  SELECT msgs.id as id, title, category.name as category, 
 			      description, source, datetime 
 				  FROM msgs, category 
 				  WHERE category.id = msgs.category 
 				  ORDER BY msgs.id DESC 
 		";
-	    $res = $this->_db->query($sql);
-	    if(!$res) return false;
-return $this->db2Arr($res);
+		$res = $this->_db->query($sql);
+		if (!$res) return false;
+		return $this->db2Arr($res);
+		
+	}
 	
-}
-function deleteNews($id){}
-function clearStr($data){
-	$data = strip_tags($data);
-	return $this->_db->escapeString($data);
-}
-function clearInt($data){
-	return abs((int)$data);
-}
+	function deleteNews($id)
+	{
+		
+	}
+	
+	function clearStr($data)
+	{
+		$data = strip_tags($data);
+		return $this->_db->escapeString($data);
+	}
+	
+	function clearInt($data)
+	{
+		return abs((int)$data);
+	}
 }
--- www/save_news.inc.php
+++ www/save_news.inc.php
@@ -9,12 +9,12 @@ $t = $news->clearStr($_POST["title"]);
 $d = $news->clearStr($_POST["description"]);
 $s = $news->clearStr($_POST["source"]);
 $c = $news->clearStr($_POST["category"]);
-if(empty($t) or empty($d)){
+if (empty($t) or empty($d)) {
 	$errMsg = "Заполните все поля формы!";
-}else{
-	if(!$news->saveNews($t, $c, $d, $s)){
+} else {
+	if (!$news->saveNews($t, $c, $d, $s)) {
 		$errMsg = "Произошла ошибка при добавлении новости";
-	}else{
+	} else {
 		header("Location: news.php");
 		exit;
 	}
