d69160c51ec9f0cd45b8cb2b0d37ddc94b6e708d
andreitimotin11 <andreitimotin11@gmail.com>
2016-12-02 19:02:09 +0200

    1-st commit

---
+++ www/NewsDB.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 26.11.2016
+ * Time: 04:11
+ */
+require_once "INewsDB.class.php";
+class NewsDB implements INewsDB;
+{
+    function saveNews($title, $category, $description, $source)
+    {
+        // TODO: Implement saveNews() method
+    }
+    function deleteNews(){
+        
+    }
+
+    function getNews(){
+
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 26.11.2016
+ * Time: 03:41
+ */
+$db = new SQLite3("test.db");
